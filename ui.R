6aba889ecac8a69893f57ac2fe4a533f34d4a41d
MalditoBarbudo <victorgrandagarcia@gmail.com>
2017-07-05 12:18:02 +0200

    ui skeleton

--- ui.R
+++ ui.R
@@ -1,1 +1,84 @@
 ## Meteoland service UI
+
+# libraries
+library(shiny)
+library(leaflet)
+library(DT)
+
+# fluid page ui
+fluidPage(
+  
+  # application title
+  titlePanel('Meteoland Service'),
+  
+  # sidebar layout. this will be a dynamic UI, where selecting between historical and
+  # projection modes will change the inputs in the sidebar
+  sidebarLayout(
+    
+    # sidebar code
+    sidebarPanel(
+      
+      # sidebar width (as default value, 4, maybe is too large)
+      width = 3,
+      
+      # mode selector. The user can select between projected or historical
+      wellPanel(
+        selectInput(
+          inputId = 'mode_sel',
+          label = 'Please select the desired mode:',
+          choices = c('Historical', 'Projection')
+        )
+      ),
+      
+      # a little spae
+      br(),
+      
+      # Dinamic ui to show selectors and buttons
+      uiOutput(
+        outputId = 'dinamic_inputs'
+      )
+    ),
+    
+    # Main panel. Here will be several tabsets (map, data, manual, examples)
+    mainPanel(
+      
+      # map tabset
+      tabsetPanel(
+        
+        # general info/aspect of tabset panel
+        id = 'main_tab_panel',
+        type = 'pills',
+        
+        # map
+        tabPanel(
+          title = 'Map',
+          icon = icon('globe'),
+          leafletOutput('map')
+        ),
+        
+        # data
+        tabPanel(
+          title = 'Data',
+          icon = icon('table'),
+          DT::dataTableOutput('data')
+        ),
+        
+        # manual
+        tabPanel(
+          title = 'Manual',
+          icon = icon('book')
+          ## TO DO select an output to show the manual (Rmd?)
+        ),
+        
+        # examples
+        tabPanel(
+          title = 'Examples',
+          icon = icon('flask')
+          ## TO DO select an output to show examples (R code or Rmd?)
+        )
+      )
+    )
+    
+  )
+  
+)
