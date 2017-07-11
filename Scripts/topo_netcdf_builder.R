8c4d507bf13326f81f2e9086dffcfe77aac7f9c9
MalditoBarbudo <victorgrandagarcia@gmail.com>
2017-07-11 18:40:32 +0200

    test for getting the nearest topology value from the provided coordinates added to topo_netcdf_builder.R script

--- Scripts/topo_netcdf_builder.R
+++ Scripts/topo_netcdf_builder.R
@@ -54,3 +54,37 @@ write_topo_grid <- function(object, file) {
 }
 
 write_topo_grid(landtopo, 'Data/Topology_grid.nc')
+
+# plotting the file with raster to take a quick look
+par(mfrow = c(2,2))
+plot(raster::raster('Data/Topology_grid.nc', varname = "Elevation")[[1]])
+plot(raster::raster('Data/Topology_grid.nc', varname = "Aspect")[[1]])
+plot(raster::raster('Data/Topology_grid.nc', varname = "Slope")[[1]])
+par(mfrow = c(1,1))
+
+
+### how to extract a variable value for an specific coordinates
+
+## read and test the file created
+nc <- nc_open('Data/Topology_grid.nc')
+
+# get x and y coordinates
+x_coord <- nc$dim$X$vals
+y_coord <- nc$dim$Y$vals
+
+# desired x and y coordinate
+desired_x <- 258030
+desired_y <- 4484980
+
+# difference for x
+dist_x <- abs(x_coord - desired_x)
+index_x <- which.min(dist_x)
+
+# difference for y
+dist_y <- abs(y_coord - desired_y)
+index_y <- which.min(dist_y)
+
+# index
+index_ok <- c(index_x, index_y)
+
+ncvar_get(nc, 'Elevation', index_ok, c(1,1))
