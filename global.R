1292eb7e7c23de0b278c3b9406c5eebd0aa71efd
MalditoBarbudo <victorgrandagarcia@gmail.com>
2017-08-02 13:35:02 +0200

    fixed complete routes to data files in the server

--- global.R
+++ global.R
@@ -76,7 +76,7 @@ getTopographyObject <- function(user_df) {
   
   for (i in 1:n_coords) {
     vals[[i]] <- ncExtractVarValueByCoords(
-      nc_file = file.path('..', 'Datasets', 'MDT', 'Topology_grid.nc'),
+      nc_file = file.path('/home', 'miquel', 'Datasets', 'MDT', 'Topology_grid.nc'),
       x_coord = user_coords_utm@coords[i,1],
       y_coord = user_coords_utm@coords[i,2],
       var_names = c('Elevation', 'Slope', 'Aspect')
@@ -131,7 +131,7 @@ current_points_mode_process <- function(user_df, user_dates,
   for (i in seq_along(datevec)) {
     # files
     day_data[[i]] <- readmeteorologypoint(
-      file.path('..', 'Datasets', 'Climate', 'Sources', 'AEMET',
+      file.path('/home', 'miquel', 'Datasets', 'Climate', 'Sources', 'AEMET',
                 'Download', 'DailyCAT',
                 paste0(as.character(datevec[[i]]), '.txt'))
     )
@@ -280,7 +280,8 @@ historical_points_mode_process <- function(user_df, user_dates,
   }
   
   # load the interpolator mother data
-  load(file.path('..', 'Datasets', 'Climate', 'Products', 'MeteorologyInterpolationData',
+  load(file.path('/home', 'miquel', 'Datasets', 'Climate', 'Products',
+                 'MeteorologyInterpolationData',
                  'Interpolator_Mother.rda'))
   
   # subset by the user dates
@@ -394,7 +395,7 @@ projection_points_mode_process <- function(user_df, rcm, rcp,
   }
   
   # folder & files paths
-  dir_path <- file.path('..', 'Datasets', 'Climate', 'Sources', 'EUROCordex')
+  dir_path <- file.path('/home', 'miquel', 'Datasets', 'Climate', 'Sources', 'EUROCordex')
   
   hist_pred_path <- file.path(dir_path, rcm, 'historical')
   future_pred_path <- file.path(dir_path, rcm, rcp)
@@ -538,7 +539,7 @@ current_grid_mode_process <- function(user_coords, user_dates,
   for (i in seq_along(datevec)) {
     # files
     day_data[[i]] <- readmeteorologypoint(
-      file.path('..', 'Datasets', 'Climate', 'Sources', 'AEMET',
+      file.path('/home', 'miquel', 'Datasets', 'Climate', 'Sources', 'AEMET',
                 'Download', 'DailyCAT',
                 paste0(as.character(datevec[[i]]), '.txt'))
     )
@@ -745,7 +746,7 @@ projection_grid_mode_process <- function(user_coords, rcm, rcp,
     )
   }
   
-  dir <- file.path('..', 'Datasets', 'Climate', 'Products', 'Pixels1k',
+  dir <- file.path('/home', 'miquel', 'Datasets', 'Climate', 'Products', 'Pixels1k',
                    'Projections', rcm, rcp)
   file <- paste0(rcm, '_', gsub('\\.', '', rcp), '.nc')
   
@@ -854,7 +855,7 @@ historical_grid_mode_process <- function(user_coords, user_dates,
                                          updateProgress = NULL) {
   
   # STEP 1 OPEN THE CONN TO THE netCDF FILE
-  file_name <- file.path('..',
+  file_name <- file.path('/home', 'miquel',
                          'Datasets', 'Climate', 'Products', 'Pixels1k',
                          'Historical', 'netCDF', 'historical_netCDF.nc')
   
